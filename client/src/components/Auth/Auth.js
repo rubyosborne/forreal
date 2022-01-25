91c7a5b717eff4a758c23c14c970feb659d8048a
rubyosborne <ruby.osborne@gmail.com>
2022-01-25 20:45:53 +1300

    getting server to work with mongo

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -79,7 +79,7 @@ const SignUp = () => {
             { isSignup ? 'Sign Up' : 'Sign In' }
           </Button>
           <GoogleLogin
-            clientId="564033717568-bu2nr1l9h31bhk9bff4pqbenvvoju3oq.apps.googleusercontent.com"
+            clientId="865397620179-3487mrohrmh5us9lvfpa42pbsp164obo.apps.googleusercontent.com"
             render={(renderProps) => (
               <Button className={classes.googleButton} color="primary" fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant="contained">
                 Google Sign In
